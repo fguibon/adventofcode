dfe0ac7efb55a0b8ec7cb5debe1b1f18efaf49db
krilexis <flying.fish@gmx.fr>
2024-01-05 09:46:04 +0100

    Init

---
+++ src/test/java/java/com/fguibon/adventofcode/AdventofcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.fguibon.adventofcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventofcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
